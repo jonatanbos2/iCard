5b3d4608c8c192e7b8def734bb345c4f1ea9f11d
jonatanbos2 <jonatanbonsosa@gmail.com>
2026-05-13 11:28:54 -0600

    creación de endpoint para obtener los datos personales

--- icard_django/icard/icard/urls.py
+++ icard_django/icard/icard/urls.py
@@ -39,5 +39,6 @@ urlpatterns = [
        'swagger', cache_timeout=0), name='schema-swagger-ui'),
    path('redocs/', schema_view.with_ui('redoc', 
         cache_timeout=0), name='schema-redoc'),
-    path('api/', include(router_user.urls)),     
+   path('api/', include('users.api.router')),
+   path('api/', include(router_user.urls)),     
 ]
--- icard_django/icard/users/api/router.py
+++ icard_django/icard/users/api/router.py
@@ -1,7 +1,13 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
-from users.api.views import UserApiViewSet
+from users.api.views import UserApiViewSet, UserView
 
 router_user = DefaultRouter()
 
 router_user.register(prefix='users', 
  viewset=UserApiViewSet, basename='users')
+
+urlpatterns = [
+    path('auth/', UserView.as_view())
+]
+
