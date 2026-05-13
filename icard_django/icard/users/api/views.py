fe685deb4cb204c17f51551f551dd32576a6e975
jonatanbos2 <jonatanbonsosa@gmail.com>
2026-05-13 10:01:12 -0600

    rutas de Users corregidas

--- icard_django/icard/users/api/views.py
+++ icard_django/icard/users/api/views.py
@@ -4,9 +4,7 @@ from users.models import User
 from users.api.serializers import UserSerializer
 
 class UserApiViewSet(ModelViewSet):
- serializer_class = UserSerializer
-queryset = User.objects.all()   
-permission_class = [IsAdminUser]
-
-
-
+    serializer_class = UserSerializer
+    
+    queryset = User.objects.all() 
+    permission_classes = [IsAdminUser]
