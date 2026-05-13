7f7aa9b705eba85b678db8e6af143699f78e8919
jonatanbos2 <jonatanbonsosa@gmail.com>
2026-05-13 17:00:00 -0600

    instalacion de paquetes yup y formik, cambios en el login en el estado y validación del formulario

--- icard_react/src/components/Admin/LoginForm/LoginForm.js
+++ icard_react/src/components/Admin/LoginForm/LoginForm.js
@@ -1,13 +1,58 @@
 import React from 'react';
 import {Button, Form} from "semantic-ui-react";
+import {useFormik} from "formik";
+import * as Yup from "yup";
 import "./LoginForm.scss";
 
+
 export function LoginForm() {
+ const formik = useFormik({
+  initialValues: initialValues(),
+  validationSchema: Yup.object(validationSchema()),
+  onSubmit: (formValue) => {
+   console.log("Formulario enviado");
+   console.log(formValue);
+  }
+});
+
   return (
-    <Form className="login-form-admin">
-    <Form.Input name="email" placeholder="Correo electronico"/>
-    <Form.Input name="password" type="password" placeholder="Contraseña" />
-    <Button type="submit" content="Iniciar Sesion" primary fluid/>
+    <Form className="login-form-admin" onSubmit={formik.handleSubmit}>
+    <Form.Input 
+    name="email" 
+    placeholder="Correo electronico" 
+    value={formik.values.email} 
+    onChange={formik.handleChange}
+    onBlur={formik.handleBlur}
+    error={formik.errors.email? {content: formik.errors.email, pointing: 'below'}:null }
+    />
+    <Form.Input name="password" type="password" 
+    placeholder="Contraseña"  
+    value={formik.values.password} 
+    onChange={formik.handleChange}
+    onBlur={formik.handleBlur}
+    error={formik.errors.password ?{content: formik.errors.password, pointing: 'below'}:null}
+    />
+    <Button type="submit" 
+    content="Iniciar Sesion" primary fluid
+    />
     </Form>
   );
+}
+
+function initialValues(){
+  return{
+      email: "",
+      password: "",
+  }
+}
+
+function validationSchema() {
+  return {
+    email: Yup.string()
+      .email("El formato parece incorrecto") // Mensaje si el formato es malo
+      .required("Necesitamos tu correo para entrar"), // Mensaje si está vacío
+    password: Yup.string()
+      .required("No olvides tu contraseña"),
+      
+  };
 }
--- icard_react/src/index.js
+++ icard_react/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import CssBaseline from '@mui/material/CssBaseline';
 import 'semantic-ui-css/semantic.min.css';
 import "./scss/global.scss";
 
