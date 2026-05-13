cffb331876853929c48ecba0004611782b48f9d6
jonatanbos2 <jonatanbonsosa@gmail.com>
2026-05-13 15:39:52 -0600

    se regreso a la version React a la 18 para ocupar Semantic UI, se genero el formulario de login y los Scss para el diseño del mismo

---
+++ icard_react/src/components/Admin/LoginForm/LoginForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Button, Form} from "semantic-ui-react";
+import "./LoginForm.scss";
+
+export function LoginForm() {
+  return (
+    <Form className="login-form-admin">
+    <Form.Input name="email" placeholder="Correo electronico"/>
+    <Form.Input name="password" type="password" placeholder="Contraseña" />
+    <Button type="submit" content="Iniciar Sesion" primary fluid/>
+    </Form>
+  );
+}
--- icard_react/src/index.js
+++ icard_react/src/index.js
@@ -1,8 +1,10 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
 import CssBaseline from '@mui/material/CssBaseline';
+import 'semantic-ui-css/semantic.min.css';
+import "./scss/global.scss";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render( <App /> );
