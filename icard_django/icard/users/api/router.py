153c61edf78218dae42a0b5bc26d3d8149478d46
jonatanbos2 <jonatanbonsosa@gmail.com>
2026-05-14 16:14:00 -0600

    implementation access token y  refresh token,

--- icard_django/icard/users/api/router.py
+++ icard_django/icard/users/api/router.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
 from users.api.views import UserApiViewSet, UserView
 
 router_user = DefaultRouter()
@@ -8,6 +11,8 @@ router_user.register(prefix='users',
  viewset=UserApiViewSet, basename='users')
 
 urlpatterns = [
-    path('auth/', UserView.as_view())
+     path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/', UserView.as_view()),
+    
 ]
 
